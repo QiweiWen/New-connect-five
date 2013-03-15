51113112f5be10139c3dfe5a8a50a08e1cb6e2f6
Qiwei Wen <qiweiwen@Qiweis-MacBook-Air.local>
2013-03-15 11:34:17 +1100

    Initial Commit

---
+++ New connect five/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  New connect five
+//
+//  Created by Qiwei Wen on 15/03/13.
+//  Copyright (c) 2013 Qiwei Wen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
